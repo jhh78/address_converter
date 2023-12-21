f28b50207943d86b15db2a87a35cc42c7144e5a8
鄭會憲 <vkcel400@gmail.com>
2023-12-21 16:43:47 +0900

    로그화면 이전 작성

---
+++ lib/core/logo.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LogoPage extends StatelessWidget {
+  const LogoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      decoration: const BoxDecoration(
+        color: Colors.deepPurple,
+      ),
+      child: const FadeInImage(
+        placeholder: AssetImage('assets/images/empty.png'),
+        image: AssetImage('assets/images/logo.png'),
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+}
