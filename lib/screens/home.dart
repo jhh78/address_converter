239ccc8c2f9df12625dc789590b17ecbc5e66042
鄭會憲 <vkcel400@gmail.com>
2023-12-24 20:27:26 +0900

    광고처리

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_core/models/db.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 //TODO:::핸드폰에 데이터 저장하기
 //TODO:::핸드폰에 데이터 불러오기
 //TODO:::핸드폰에 데이터 삭제하기
 //TODO:::API 연동하기
 //TODO:::UI만들기
-//TODO:::광고달기
-//TODO:::인앱 결제 넣기
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -20,56 +19,93 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Map<String, dynamic>> jsonList = [];
   DBModel dbModel = DBModel();
 
+  BannerAd? _bannerAd;
+  bool _isLoaded = false;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     jsonList = dbModel.doSelect();
+    loadAd();
+  }
+
+  void loadAd() {
+    _bannerAd = BannerAd(
+      adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+      request: const AdRequest(),
+      size: AdSize.banner,
+      listener: BannerAdListener(
+        // Called when an ad is successfully received.
+        onAdLoaded: (ad) {
+          debugPrint('>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $ad loaded.');
+          setState(() {
+            _isLoaded = true;
+          });
+        },
+        // Called when an ad request failed.
+        onAdFailedToLoad: (ad, err) {
+          debugPrint('>>>>>>>>>>>>>>>>>> BannerAd failed to load: $err');
+          // Dispose the ad here to free resources.
+          ad.dispose();
+        },
+      ),
+    )..load();
   }
 
   @override
   Widget build(BuildContext context) {
-    print(jsonList);
+    debugPrint('into build $jsonList');
     return SafeArea(
       child: Scaffold(
-          body: Column(
-        children: [
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '入力した住所を英語へ変換します。',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+        appBar: AppBar(
+          title: const Text('英語住所変換'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                  decoration: const InputDecoration(
+                    alignLabelWithHint: true,
+                    border: OutlineInputBorder(),
+                    labelText: '変換する住所を入力してください。',
+                  ),
+                  onSubmitted: (String searchWord) {
+                    // TODO::: API처리
+                    debugPrint(
+                        '???????????????????????????????????????? $searchWord');
+                  },
+                  textInputAction: TextInputAction.search,
+                  maxLines: 5,
+                  minLines: 5),
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemCount: jsonList.length,
+                itemBuilder: (context, builder) {
+                  return ListTile(
+                    title: Text(jsonList[builder].toString()),
+                  );
+                },
+                separatorBuilder: (context, builder) {
+                  return const Divider(
+                    height: 2,
+                  );
+                },
               ),
-            ],
-          ),
-          const TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: '変換する住所を入力してください。',
-            ),
-            textInputAction: TextInputAction.search,
-          ),
-          Expanded(
-            child: ListView.separated(
-              itemCount: jsonList.length,
-              itemBuilder: (context, builder) {
-                return ListTile(
-                  title: Text(jsonList[builder].toString()),
-                );
-              },
-              separatorBuilder: (context, builder) {
-                return const Divider(
-                  height: 2,
-                );
-              },
-            ),
-          )
-        ],
-      )),
+            )
+          ],
+        ),
+        bottomNavigationBar: _isLoaded
+            ? SizedBox(
+                height: _bannerAd!.size.height.toDouble(),
+                width: _bannerAd!.size.width.toDouble(),
+                child: AdWidget(ad: _bannerAd!),
+              )
+            : const SizedBox(),
+      ),
     );
   }
 }
